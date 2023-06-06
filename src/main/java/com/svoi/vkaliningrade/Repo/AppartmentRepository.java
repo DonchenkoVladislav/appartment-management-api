a7d7b79dd3ed6d5691fc1f39a60fde9e6e220596
d.emelyanov <emelyanov.dem@gmail.com>
2023-06-06 21:35:17 +0300

    Add Model and Repo Apartments. Add gitinore.

---
+++ src/main/java/com/svoi/vkaliningrade/Repo/AppartmentRepository.java
@@ -0,0 +1,7 @@
+package com.svoi.vkaliningrade.Repo;
+
+import com.svoi.vkaliningrade.Models.Appartment;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AppartmentRepository extends CrudRepository<Appartment, Long> {
+}
