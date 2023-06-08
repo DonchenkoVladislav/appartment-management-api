0b76ab4c7e2875227e198d4c1046fec9b6ec97ef
novelect <donchenkovladislav@list.ru>
2023-06-09 00:40:26 +0300

    Исправление ошибки

--- src/main/java/com/svoi/vkaliningrade/Controllers/AppartmentAdminController.java
+++ src/main/java/com/svoi/vkaliningrade/Controllers/AppartmentAdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppartmentAdminController {
 
-    @GetMapping("/appartment")
+    @GetMapping("/appartments-add")
     public String getAppartmentAdminEditPage() {
         return "appartmentedit";
     }
