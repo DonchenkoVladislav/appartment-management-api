471365133fe6e92ef76c845f58b9412b2a61cc34
novelect <donchenkovladislav@list.ru>
2023-06-16 17:36:57 +0300

    Создал HtmlFormController

---
+++ src/main/java/com/svoi/vkaliningrade/Controllers/HtmlFormController.java
@@ -0,0 +1,25 @@
+package com.svoi.vkaliningrade.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HtmlFormController {
+
+    /*Контроллер только для отправки html кода,
+    чтоб не писать весь html в одном файле,
+    а делать запросы к этому контроллеру
+    и подставлять его в нужные элементы на странице*/
+
+    /*Контроллер для получения формы добавления/изменения объектов*/
+    @GetMapping("/adding-form")
+    public String getApartmentAddForm() {
+        return "forms/adding_form";
+    }
+
+    /*Контроллер для получения формы фильтров*/
+    @GetMapping("/filter-form")
+    public String getFilterForm() {
+        return "forms/filter_form";
+    }
+}
---
+++ src/main/resources/static/scripts/requests.js
@@ -0,0 +1,107 @@
+function save(formData) {
+
+    let tariffList = new Array()
+
+    Array.from(document.getElementsByClassName("tariff")).forEach((item) => {
+
+        var tariff = new Object()
+
+        tariff.startDate = item.querySelector("[name=input_startDate]").value
+        tariff.endDate = item.querySelector("[name=input_endDate]").value
+        tariff.summaryTariff = item.querySelector("[name=input_summary_tariff]").value
+
+        tariffList.push(tariff)
+    })
+
+    let requiredFields = [
+        $("[name='input_name']").val(),
+        $("[name='input_coordinates']").val(),
+        $("[name='input_space']").val(),
+        $("[name='input_adult']").val(),
+        $("[name='input_beds']").val(),
+        $("[name='input_summary']").val()
+    ]
+
+    let allrequiredFieldsIsFilled = true
+
+    requiredFields.forEach(element => {
+        if (element === "") {
+            allrequiredFieldsIsFilled = false
+        }
+    })
+
+    if (allrequiredFieldsIsFilled) {
+        //Отправка нового объекта на сервер
+        $.ajax({
+            type: 'POST',
+            url: '/appartments-add',
+            dataType: 'json',
+            processData: false,
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify({
+                "name": $("[name='input_name']").val(),
+                "city": $("[name='input_city']").val(),
+                "coordinates": $("[name='input_coordinates']").val(),
+                "space": $("[name='input_space']").val(),
+                "adult": $("[name='input_adult']").val(),
+                "children": $("[name='input_children']").val(),
+                "beds": $("[name='input_beds']").val(),
+                "from": $("[name='input_from']").val(),
+                "summary": $("[name='input_summary']").val(),
+                "view": $("[name='input_view']").val(),
+                "conveniences": $("[name='input_conveniences']").val(),
+                "services": $("[name='input_services']").val(),
+                "tariffs": tariffList,
+            }),
+            error: function (request, status, error) {
+                if (!request.status === 200) {
+                    alert("Упс! Что-то пошло не так. Попробуйте еще раз")
+                }
+            }
+        })
+
+        formData.append("appartmentName", $("[name='input_name']").val())
+
+        //Отправка фотографий нового объекта на сервер
+        $.ajax({
+            type: 'POST',
+            url: '/appartments-add-photos',
+            dataType: 'json',
+            processData: false,
+            contentType: 'application/json; charset=utf-8',
+            data: formData,
+            error: function (request, status, error) {
+                if (!request.status === 200) {
+                    alert("Не удалось загрузить изображения")
+                }
+            }
+        })
+    } else {
+        alert("Заполнены не все обязательные поля")
+    }
+}
+
+// function getJson(url) {
+//     //Получение объекта для формирования списка со всеми записями
+//     $.ajax({
+//         url: url,
+//         type: 'GET',
+//         headers: {
+//             'Accept': 'application/json'
+//         },
+//         success: function(all) {
+//             return all
+//         },
+//         error: function(jqXHR, textStatus, errorThrown) {
+//             // Обработка ошибки
+//         }
+//     });
+// }
+
+function getJson(url) {
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.send()
+
+    return xhr.response
+}
