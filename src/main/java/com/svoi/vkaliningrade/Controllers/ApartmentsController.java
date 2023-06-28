8719160db3a1c56cd66a05e8f3d4036cb85dcc93
d.emelyanov <emelyanov.dem@gmail.com>
2023-06-28 21:56:44 +0300

    Добавлен метод сохранения в БД данных из формы редактирования объекта

--- src/main/java/com/svoi/vkaliningrade/Controllers/ApartmentsController.java
+++ src/main/java/com/svoi/vkaliningrade/Controllers/ApartmentsController.java
@@ -52,4 +52,10 @@ public class ApartmentsController {
        return apartmentsService.getApartment(id);
     }
 
+    @PutMapping("/edit")
+    public String edit(@RequestParam(defaultValue = "0") Long id, @RequestBody ApartmentInfo requestBody){
+        apartmentsService.edit(id, requestBody);
+        return "main0";
+    }
+
 }
